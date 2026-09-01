d06f0ad0f5715ee559382246a85262c7eac1d8ac
William Skou Heidemann <Williamskou2227@gmail.com>
2026-09-01 16:26:34 +0200

    week 2 exercises

--- 02-adt/Exercises.scala
+++ 02-adt/Exercises.scala
@@ -34,72 +34,105 @@ object List:
 
   // Exercise 1 (is to be solved without programming)
 
+  // Answer: 1. The list will match with head and a tail and the expression evaluates to the head.
+
   // Exercise 2
 
-  def tail[A] (l: List[A]): List[A] = ???
+  def tail[A] (l: List[A]): List[A] = l match
+    case Nil => throw NoSuchElementException()
+    case Cons(head, tail) => tail
 
   // Exercise 3
   
-  def drop[A] (l: List[A], n: Int): List[A] = ???
+  def drop[A] (l: List[A], n: Int): List[A] =
+    if n <= 0 then l
+    else l match
+      case Nil => throw NoSuchElementException()
+      case Cons(head, tail) => drop(tail, n-1)
 
   // Exercise 4
 
-  def dropWhile[A] (l: List[A], p: A => Boolean): List[A] = ???
+  def dropWhile[A] (l: List[A], p: A => Boolean): List[A] = l match
+    case Nil => Nil
+    case Cons(head, tail) => if p(head) then dropWhile(tail, p) else l
 
   // Exercise 5
- 
-  def init[A] (l: List[A]): List[A] = ???
+  def init[A] (l: List[A]): List[A] = l match
+    case Nil => throw NoSuchElementException()
+    case Cons(head, Nil) => Nil
+    case Cons(head, Cons(head2, Nil)) => Cons(head, Nil)
+    case Cons(head, tail) => Cons(head, init(tail))
 
   // Exercise 6
 
-  def length[A] (l: List[A]): Int = ???
+  def length[A] (l: List[A]): Int =
+    foldRight[A, Int] (l, 0, (element, acc) => acc + 1)
 
   // Exercise 7
 
-  def foldLeft[A, B] (l: List[A], z: B, f: (B, A) => B): B = ???
+  @annotation.tailrec
+  def foldLeft[A, B] (l: List[A], z: B, f: (B, A) => B): B = l match
+    case Nil => z
+    case Cons(head, tail) => foldLeft[A, B] (tail, f(z, head), f)
 
   // Exercise 8
 
-  def product (as: List[Int]): Int = ???
+  def product (as: List[Int]): Int =
+    foldLeft[Int, Int] (as, 1, (acc, element) => acc * element)
 
-  def length1[A] (as: List[A]): Int = ???
+  def length1[A] (as: List[A]): Int =
+    foldLeft[A, Int] (as, 0, (acc, element) => acc + 1)
 
   // Exercise 9
 
-  def reverse[A] (l: List[A]): List[A] = ???
+  def reverse[A] (l: List[A]): List[A] =
+    foldLeft[A, List[A]] (l, Nil, (acc, element) => Cons(element, acc))
  
   // Exercise 10
 
-  def foldRight1[A, B] (l: List[A], z: B, f: (A, B) => B): B = ???
+  def foldRight1[A, B] (l: List[A], z: B, f: (A, B) => B): B =
+    foldLeft[A, B] (reverse(l), z, (acc, element) => f(element, acc))
 
   // Exercise 11
 
   def foldLeft1[A, B] (l: List[A], z: B, f: (B, A) => B): B = ???
+    // foldRight[A, B => B]
  
   // Exercise 12
 
-  def concat[A] (l: List[List[A]]): List[A] = ???
-  
+  def concat[A] (l: List[List[A]]): List[A] =
+    foldLeft[List[A], List[A]] (l, Nil, (acc, element) => append(acc, element))
+
   // Exercise 13
 
-  def filter[A] (l: List[A], p: A => Boolean): List[A] = ???
- 
+  def filter[A] (l: List[A], p: A => Boolean): List[A] =
+    foldRight[A, List[A]] (l, Nil, (element, acc) => if p(element) then Cons(element, acc) else acc)
+
   // Exercise 14
 
-  def flatMap[A,B] (l: List[A], f: A => List[B]): List[B] = ???
+  def flatMap[A,B] (l: List[A], f: A => List[B]): List[B] =
+    foldLeft[A, List[B]] (l, Nil, (acc, element) => append(acc, f(element)))
 
   // Exercise 15
 
-  def filter1[A] (l: List[A], p: A => Boolean): List[A] = ???
+  def filter1[A] (l: List[A], p: A => Boolean): List[A] =
+    flatMap[A, A] (l, element => if p(element) then Cons(element, Nil) else Nil)
 
   // Exercise 16
 
-  def addPairwise (l: List[Int], r: List[Int]): List[Int] = ???
+  def addPairwise (l: List[Int], r: List[Int]): List[Int] = l match
+    case Nil => Nil
+    case Cons(lHead, lTail) => r match
+      case Nil => Nil
+      case Cons(rHead, rTail) => Cons(lHead + rHead, addPairwise(lTail, rTail))
 
   // Exercise 17
 
-  def zipWith[A, B, C] (l: List[A], r: List[B], f: (A,B) => C): List[C] = ???
-
+  def zipWith[A, B, C] (l: List[A], r: List[B], f: (A,B) => C): List[C] = l match
+    case Nil => Nil
+    case Cons(lHead, lTail) => r match
+      case Nil => Nil
+      case Cons(rHead, rTail) => Cons(f(lHead, rHead), zipWith(lTail, rTail, f))
   // Exercise 18
 
   def hasSubsequence[A] (sup: List[A], sub: List[A]): Boolean = ???
