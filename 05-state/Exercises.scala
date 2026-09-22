471ffc75d713851527deb6c5fd753e366e987773
William Skou Heidemann <Williamskou2227@gmail.com>
2026-09-22 16:10:07 +0200

    05-state exercise 5-9. Not all tests pass

--- 05-state/Exercises.scala
+++ 05-state/Exercises.scala
@@ -61,10 +61,11 @@ object RNG:
   // The return type is broken and needs to be fixed
   def ints(size: Int)(rng: RNG): (List[Int], RNG) = {
     def recurse(acc: List[Int], x: Int, rng: RNG): (List[Int], RNG) =
-      if x <= 0 then (acc, rng)
+      if x <= 0 then (acc.reverse, rng)
       else
         val (i, r1) = rng.nextInt
         recurse(i :: acc, x - 1, r1)
+
     recurse(List.empty, size, rng)
   }
 
@@ -76,38 +77,51 @@ object RNG:
   def unit[A](a: A): Rand[A] = rng => (a, rng)
 
   def map[A, B](s: Rand[A])(f: A => B): Rand[B] =
-    rng => {
+    rng =>
       val (a, rng2) = s(rng)
       (f(a), rng2)
-    }
+
 
   def nonNegativeEven: Rand[Int] = map(nonNegativeInt) { i => i - i % 2 }
 
   // Exercise 5
 
   lazy val double2: Rand[Double] =
-    ???
+    map[Int, Double](RNG.int)(i =>
+      val iPositive = if i == Int.MinValue then 0 else i.abs
+      iPositive.toDouble / Int.MaxValue.toDouble)
 
   // Exercise 6
 
   def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
-    ???
+    rng =>
+      val (a, r1) = ra(rng)
+      val (b, r2) = rb(r1)
+      val c = f(a, b)
+      (c, r2)
 
   // Exercise 7
 
   def sequence[A](ras: List[Rand[A]]): Rand[List[A]] =
-    ???
+    ras.foldRight(unit(List.empty[A]))((ra, acc) => map2(ra, acc)((a, as) => a :: as))
 
-  def ints2(size: Int): Rand[List[Int]] =
-    ???
+  def ints2(size: Int): Rand[List[Int]] = {
+    val initial: List[Rand[Int]] = List.fill[Rand[Int]](size)(int)
+    sequence(initial)
+  }
 
   // Exercise 8
 
   def flatMap[A, B](f: Rand[A])(g: A => Rand[B]): Rand[B] =
-    ???
+    rng =>
+      val (a, r1) = f(rng)
+      g(a)(r1)
 
   def nonNegativeLessThan(bound: Int): Rand[Int] =
-    ???
+    flatMap(nonNegativeInt)(i =>
+      val iBound = i % bound
+      rng => (iBound, rng)
+    )
 
 end RNG
 
@@ -118,14 +132,30 @@ case class State[S, +A](run: S => (A, S)):
   // Exercise 9 (methods in class State)
   // Search for the second part (sequence) below
 
-  def flatMap[B](f: A => State[S, B]): State[S, B] =
-    ???
+  def flatMap[B](f: A => State[S, B]): State[S, B] = {
+    State {
+      s =>
+        val (a, s1) = run(s)
+        val s2 = f(a)
+        s2.run(s1)
+    }
+  }
 
   def map[B](f: A => B): State[S, B] =
-    ???
+    State {
+      s =>
+        val (a, s1) = run(s)
+        val b = f(a)
+        (b, s1)
+    }
 
   def map2[B, C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
-    ???
+    State {
+      s =>
+        val (a, s1) = run(s)
+        val (b, s2) = sb.run(s1)
+        (f(a, b), s2)
+    }
 
 
 object State:
@@ -149,7 +179,7 @@ object State:
   // Exercise 9 (sequence, continued)
 
   def sequence[S, A](sas: List[State[S, A]]): State[S, List[A]] =
-    ???
+    sas.foldRight[State[S, List[A]]](unit(List.empty))((s, acc) => s.map2(acc)((a, b) => a::b))
 
   import adpro.lazyList.LazyList
 
